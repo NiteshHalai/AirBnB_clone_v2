19eef93649a56646be25d512605269b3cbf76796
NiteshHalai <nitesh.halai@gmail.com>
2023-03-21 23:31:05 +0300

    Update console.py

--- console.py
+++ console.py
@@ -112,7 +112,23 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Overrides the emptyline method of CMD """
         pass
+    
+    def do_create(self, arg):
+        """Creates a new instance of a class"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            new_dict = self._key_value_parser(args[1:])
+            instance = classes[args[0]](**new_dict)
+        else:
+            print("** class doesn't exist **")
+            return False
+        print(instance.id)
+        instance.save()
 
+        '''
     def do_create(self, args):
         """ Create an object of any class"""
         if not args:
@@ -125,7 +141,7 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
         print(new_instance.id)
         storage.save()
-
+'''
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
