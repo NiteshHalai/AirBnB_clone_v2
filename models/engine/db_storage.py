d030e1df74a763b24e66fdca2c6dc0b6303a710b
NiteshHalai <nitesh.halai@gmail.com>
2023-03-23 00:01:32 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -8,14 +8,48 @@ class DBStorage:
     __engine = None
     __session = None
 
-    
     def __init__(self):
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.
                                       format(getenv('HBNB_MYSQL_USER'),
                                              getenv('HBNB_MYSQL_PWD'),
-                                             getenv('HBNB_MYSQL_HOST'),
+                                             getenv('HBNB_MYSQL_HOST', 'localhost'),
                                              getenv('HBNB_MYSQL_DB')),
                                       pool_pre_ping=True)
 
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        obj_dict = {}
+        classes = ['User', 'State', 'City', 'Amenity', 'Place', 'Review']
+
+        if cls:
+            if type(cls) == str:
+                cls = eval(cls)
+            query = self.__session.query(cls).all()
+            for obj in query:
+                key = '{}.{}'.format(type(obj).__name__, obj.id)
+                obj_dict[key] = obj
+        else:
+            for c in classes:
+                query = self.__session.query(eval(c)).all()
+                for obj in query:
+                    key = '{}.{}'.format(c, obj.id)
+                    obj_dict[key] = obj
+
+        return obj_dict
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        self.__session = scoped_session(sessionmaker(bind=self.__engine,
+                                                     expire_on_commit=False))()
