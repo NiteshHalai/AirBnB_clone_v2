4dc660607d2730d96a446afda15db3798f69c984
NiteshHalai <nitesh.halai@gmail.com>
2023-03-23 00:34:06 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """getter attribute in case of filestorage"""
+            from models import storage
+            from models.city import City
+            all_cities = storage.all(City)
+            cities_list = []
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
