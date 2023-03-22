4d2745d930f167e2b679e0566123641a435e3970
NiteshHalai <nitesh.halai@gmail.com>
2023-03-22 23:57:02 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,1 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from os import getenv
 
+
+class DBStorage:
+    __engine = None
+    __session = None
